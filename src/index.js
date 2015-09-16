caa8c667a4901d0be75701c94ab6e060abc38e7b
jbenkual <joe@benkual.com>
2015-09-16 15:35:50 -0700

    Add Twitter Authentication
    Allow users to authenticate using the Twitter API with Firebase storing the auth token

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-let hot = "HOT";
-console.log(`ES2015 is ${hot}`);
+var ref = new Firebase("https://tweettogether.firebaseio.com");
+
+let authorizeGuests = () => {}
+	if(localStorage["firebase:session::tweettogether"]) {
+		return;
+}
+
+ref.authWithOAuthPopup("twitter", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    alert("Authenticated successfully with payload:", authData);
+  }
+});
+
+authorizeGuests();
